f935ae374062f1b34c629c98b50a1c9a52e36a0d
Kallu-A <lucas.aries@protonmail.com>
2023-02-20 23:01:16 +0100

    feat front: scroll bar

--- front/pages/armoire.js
+++ front/pages/armoire.js
@@ -56,82 +56,84 @@ export default function armoire() {
         <>
             <ModalGame game={data_game} isShow={show} setShow={setShow}/>
 
-            <div className="vertical-bar bg-grey-litle-plain padding-bar">
-                <h1 className='text-xl center-text margin-title title-color'>Filtre</h1>
-                <hr className=''></hr>
+            <div className="vertical-bar bg-grey-litle-plain">
+                <div className='scrollable-vertical-filter padding-bar'>
+                    <h1 className='text-xl center-text margin-title title-color'>Filtre</h1>
+                    <hr className=''></hr>
 
-                <p className='margin-top-xs'>Nom du jeu</p>
-                <input className='input-name'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                />
+                    <p className='margin-top-xs'>Nom du jeu</p>
+                    <input className='input-name'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                    />
 
-                <input className='input-player margin-top-xs'
-                        value={player}
-                        onChange={(e) => setPlayer(e.target.value)}
-                /> <span>Joueurs</span>
+                    <input className='input-player margin-top-xs'
+                            value={player}
+                            onChange={(e) => setPlayer(e.target.value)}
+                    /> <span>Joueurs</span>
 
-                <input className='input-duration margin-top-xs'
-                        value={duration}
-                        onChange={(e) => setDuration(e.target.value)}
-                /> <span>Minutes</span>
+                    <input className='input-duration margin-top-xs'
+                            value={duration}
+                            onChange={(e) => setDuration(e.target.value)}
+                    /> <span>Minutes</span>
 
-                <p className='margin-top-xs'>Difficulté</p>
-                <select
-                       value={difficulty}
-                       onChange={(e) => setDifficulty(e.target.value)}
-                        className='input-difficulty'>
+                    <p className='margin-top-xs'>Difficulté</p>
+                    <select
+                           value={difficulty}
+                           onChange={(e) => setDifficulty(e.target.value)}
+                            className='input-difficulty'>
 
-                    <option value='' label="aucun choix"></option>
-                    <option value='1' label="facile"></option>
-                    <option value='2' label="moyen"></option>
-                    <option value='3' label="difficile"></option>
-                    <option value='4' label="très difficile"></option>
-                </select>
+                        <option value='' label="aucun choix"></option>
+                        <option value='1' label="facile"></option>
+                        <option value='2' label="moyen"></option>
+                        <option value='3' label="difficile"></option>
+                        <option value='4' label="très difficile"></option>
+                    </select>
 
-                <hr className='margin-top-xs'></hr>
+                    <hr className='margin-top-xs'></hr>
 
-                <h1 className='margin-top-xs text-small center-text title-color'>Catégories</h1>
-                <div className="flex flex-column margin-top-xs justify-center">
+                    <h1 className='margin-top-xs text-small center-text title-color'>Catégories</h1>
+                    <div className="flex flex-column margin-top-xs justify-center">
 
-                    { category_values.map( (value, index) => {
-                        return (
-                            <div key={value} className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
-                                <input
-                                    className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-[rgba(0,0,0,0.25)] bg-white outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:bg-white focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent"
-                                    type="checkbox"
-                                    value={categories[index][0]}
-                                    checked={categories[index][0]}
-                                    onChange={(e) => {
-                                        categories[index][1](e.target.checked)
-                                        mutate()
-                                    }}
-                                    id="checkboxDefault"/>
-                                <label
-                                    className="inline-block pl-[0.15rem] margin-left-s hover:cursor-pointer"
-                                    htmlFor="checkboxDefault">
-                                    {value}
-                                </label>
-                            </div>
-                        )
-                    })}
+                        { category_values.map( (value, index) => {
+                            return (
+                                <div key={value} className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+                                    <input
+                                        className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-[rgba(0,0,0,0.25)] bg-white outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:bg-white focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent"
+                                        type="checkbox"
+                                        value={categories[index][0]}
+                                        checked={categories[index][0]}
+                                        onChange={(e) => {
+                                            categories[index][1](e.target.checked)
+                                            mutate()
+                                        }}
+                                        id="checkboxDefault"/>
+                                    <label
+                                        className="inline-block pl-[0.15rem] margin-left-s hover:cursor-pointer"
+                                        htmlFor="checkboxDefault">
+                                        {value}
+                                    </label>
+                                </div>
+                            )
+                        })}
 
-                </div>
+                    </div>
 
-                <button className='button-style button-smaller margin-top-xs' onClick={() => {
-                    setDuration('')
-                    setPlayer('')
-                    setDifficulty('')
-                    setName('')
-                    for (let i = 0; i < categories.length; i++) {
-                        categories[i][1](false)
-                        console.log('issue' + categories[i][0])
+                    <button className='button-style button-smaller margin-top-xs' onClick={() => {
+                        setDuration('')
+                        setPlayer('')
+                        setDifficulty('')
+                        setName('')
+                        for (let i = 0; i < categories.length; i++) {
+                            categories[i][1](false)
+                            console.log('issue' + categories[i][0])
+                        }
+                        mutate()
                     }
-                    mutate()
-                }
-                }>
-                    Réinitialiser
-                </button>
+                    }>
+                        Réinitialiser
+                    </button>
+                </div>
             </div>
 
             <div className='scrollable-vertical decal-vertical-bar'>
