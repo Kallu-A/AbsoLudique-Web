0d0a8d331ba62dcb5d53acbc33f903f26571a6f7
Kallu-A <lucas.aries@protonmail.com>
2023-03-19 10:06:06 +0100

    front: fix bug display responsive

--- front/components/navbar.jsx
+++ front/components/navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
         { navigationRoutes.length !== 0 &&
 
         <button
-          className=' inline-flex p-3 hover:bg-grey-medium rounded-0p25 lg:hidden text-white ml-auto hover:text-white outline-none'
+          className='inline-flex p-3 hover:bg-grey-medium rounded-0p25 lg:hidden text-white ml-auto hover:text-white outline-none'
           onClick={handleClick}
         >
           <svg
