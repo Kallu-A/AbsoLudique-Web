7db3d01ac76e5f7d4c9b197573c83602d20440f2
Kallu-A <lucas.aries@protonmail.com>
2023-03-27 22:14:11 +0200

    add: name

--- front/pages/login.js
+++ front/pages/login.js
@@ -24,18 +24,20 @@ export default function Login() {
     return (
         <>
             <div className="inset-backdrop-not-nav overflow-y-auto backdrop">
-                <div className='centered-element padding-35-60 shadow-el rounded-0p75 bg-white'>
+                <div className='centered-element padding-35-60-0 shadow-el rounded-0p75 bg-white'>
                     <Image src="/logo.svg" alt="logo d'abso'ludique" width="250" height="250" />
                     <div className=' padding-1 text-center'>
                         <h1 className='title-color text-2'>Abso'Ludique</h1>
                         <p className='text-1p6'>Télécom Nancy</p>
-                        <div className='margin-top-50 button-smaller padding-20 button-style center-button'>
+                        <div className='margin-top-35 button-smaller padding-20 button-style center-button'>
                             <button className='margin-10' onClick={login}>
                                 Connexion avec<br></br>
                                 @telecomnancy.net
                             </button>
                         </div>
-
+                    </div>
+                    <div className="mt-4 mb-2 text-center">
+                        Réalisé par <b><a className="link-color" href="https://github.com/Kallu-A">Kallu</a></b>
                     </div>
                 </div>
             </div>
